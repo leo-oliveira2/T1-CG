3eff8aa9900fc3a34a7d4921e5c711e543aa5556
Leonardo Heinen Oliveira <108004374+leo-oliveira2@users.noreply.github.com>
2023-08-24 18:17:06 -0300

    Add files via upload

---
+++ DiagramaVoronoi.h
@@ -0,0 +1,33 @@
+//
+//  DiagramaVoronoi.hpp
+//  OpenGLTest
+//
+//  Created by Márcio Sarroglia Pinho on 23/08/23.
+//  Copyright © 2023 Márcio Sarroglia Pinho. All rights reserved.
+//
+
+#ifndef DiagramaVoronoi_h
+#define DiagramaVoronoi_h
+
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+#include "Poligono.h"
+
+
+class Voronoi
+{
+    Poligono Diagrama[1000];
+    unsigned int qtdDePoligonos;
+    Ponto Min, Max;
+public:
+    Voronoi();
+    Poligono LeUmPoligono();
+    void LePoligonos(const char *nome);
+    Poligono getPoligono(int i);
+    void obtemLimites(Ponto &min, Ponto &max);
+    unsigned int getNPoligonos();
+};
+
+#endif /* DiagramaVoronoi_h */
